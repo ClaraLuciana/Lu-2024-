9d5e4044431c46879ea97205874cfdca17f068eb
Luciana Clara Silva <114404204+ClaraLuciana@users.noreply.github.com>
2024-08-08 22:32:24 -0300

    08/08

--- main.js
+++ main.js
@@ -19,4 +19,6 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo);
